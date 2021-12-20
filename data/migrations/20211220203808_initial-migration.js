439c5b2d270adfa87ea4440eb3d53284eaee37b8
cainpalmer <cain.a.palmer@gmail.com>
2021-12-20 15:57:07 -0500

    made tables

--- data/migrations/20211220203808_initial-migration.js
+++ data/migrations/20211220203808_initial-migration.js
@@ -3,14 +3,21 @@ exports.up = function(knex) {
     return knex.schema
     .createTable('users', users => {
       users.increments('user_id')
-      users.string('username', 128).notNullable().unique()
-      users.string('password', 128).notNullable()
+      users.string('user_name', 128).notNullable().unique()
+      users.string('user_password', 128).notNullable()
     })
     .createTable('plants', plants => {
       plants.increments('plant_id')
       plants.string('plant_name', 128).notNullable().unique()
       plants.string('plant_species', 128).notNullable()
       plants.string('plant_frequency', 32).notNullable()
+      plants.integer('user_id', )
+      .unsigned()
+      .notNullable()
+      .references('user_id')
+      .inTable('users')
+      .onUpdate('RESTRICT')
+      .onDelete('RESTRICT')
     })
 };
 
